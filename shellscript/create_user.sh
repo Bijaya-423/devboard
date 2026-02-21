86169af7f2ad86c735512c532e619fa14421f226
Ubuntu <ubuntu@ip-172-31-31-129.ec2.internal>
2026-02-21 12:20:59 +0000

    argument

---
+++ shellscript/create_user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+read -p "Enter User Name: " username
+
+echo "You Entered $username"
+
+sudo useradd -m $username
+
+echo "User Added Through Prompt"
+
+echo "The characters in $0 $1 $2" #this is the arguments at the time of run the bash script in shell you enter ./create_user.sh hello devops cloud
+
+
+
+
+
+
+
+#read -p "Enter the group: " $1
+
+#echo "You Enter the group name through args $1"
+
+sudo groupadd $1    # i want to add the group through the arguments 
+echo "The characters in $0 $1"
+
