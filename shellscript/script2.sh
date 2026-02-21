99f6426a853e060b1c47ed31bea4022cfd980250
Ubuntu <ubuntu@ip-172-31-31-129.ec2.internal>
2026-02-21 11:47:57 +0000

    shell script

---
+++ shellscript/script2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+#This is single line comment
+
+<< comment
+
+This is
+multiline 
+commentt
+
+
+Anything written
+here 
+will not be
+execute
+
+comment
+
+first_name="Bijaya"
+middle_name="Kumar"
+last_name="Jena"
+echo "Name is :- $first_name " $middle_name $last_name
+
+
+fullname="Bijaya kumar jena"
+echo "Name is $fullname, and date is $(date)"
+
+echo "Enter the name: "
+
+read username
+
+echo "You entered $username"
+
+
+
+
+
+
+
+
+
+
+read -p "What is DevOps: " Devops
+echo "Hello $Devops"
+
+
+
+
+
+
+
+
+
+
+
