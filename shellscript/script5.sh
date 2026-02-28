c0060e041af32b272c2137005891e027fe5a0df3
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-02-28 22:31:25 +0530

    shell script

--- shellscript/script5.sh
+++ shellscript/script5.sh
@@ -9,7 +9,25 @@ echo "hello azure" > helloazure.txt
 
 
 
+echo "hello gcp" > hellogcp.txt
+echo "hello devops" > hellodevops.txt
+if [ -f "helloaws.txt" ]; then
+    echo "File helloaws.txt exists."
+else
+    echo "File helloaws.txt does not exist."
+fi
 
+if [ -f "helloazure.txt" ]; then
+    echo "File helloazure.txt exists."
+else
+    echo "File helloazure.txt does not exist."
+fi
+
+if [ -f "hellogcp.txt" ]; then
+    echo "File hellogcp.txt exists."
+else
+    echo "File hellogcp.txt does not exist."
+fi
 
 
 
