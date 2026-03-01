8888d7d0c5da2e709755a16cf2fc78557b507ce9
Ubuntu <ubuntu@ip-172-31-31-129.ec2.internal>
2026-03-01 10:04:53 +0000

    shell script

---
+++ shellscript/script9.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CICD="Jenkins"
+
+read -p "Enter the Devops Course Details: " devops
+echo
+echo "ci/cd - $CICD" , $devops
+echo
+read -p "arguments pass in script run time $1"
+
+
+
