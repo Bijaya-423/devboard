0c4ab5183f8bfa10efb45edc8ebbb1be07e26e85
Ubuntu <ubuntu@ip-172-31-31-129.ec2.internal>
2026-03-02 12:41:10 +0000

    loops

---
+++ shellscript/condition.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+<<disclaimer
+THis is 
+for the 
+devops 
+condtional 
+statement
+
+disclaimer
+
+
+read -p "enter the course: " course
+read -p "enter the azure %: " cloud
+
+echo
+
+if [[ $course == "aws" ]]
+then
+	echo "devops course"
+elif [[ $cloud -ge 100 ]];
+then
+	echo "you know azure"
+else
+	echo "not devops course"
+
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ shellscript/loops.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#This is for & while loops
+
+read -p "Enter How Many folder You create: " folder
+echo
+for ((i=1 ; i<=folder; i++));
+do
+	#mkdir "demo$i"
+#	echo "demo$i folder created."
+
+
+
+<<comment
+	if [[ i=$i ]]
+	then
+		echo "demo$i folder created."
+	fi
+comment
+
+
+
+	if [[ -d "demo$i" ]]
+	then 
+		echo "demo$i folder already exists."
+		rm -r "demo$i"
+	#else
+	#	mkdir "demo$i"
+	#	echo "demo$i folder created."
+	fi
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
