78b09dbfecad676b3c1e252c291410931d0c24f5
Ubuntu <ubuntu@ip-172-31-31-129.ec2.internal>
2026-03-02 14:49:17 +0000

    loops

---
+++ shellscript/loops3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#This is for and while loops
+#
+#
+<<comment
+ 1 - argument 1 which is folder name
+ 2 - is start range
+ 3 - is end range
+comment
+
+for (( i=$2; i<=$3; i++ ))
+do
+       if [[ -d "$1$i" ]]
+       then
+           echo "dir exists"	       
+       else
+	   mkdir "$1$i"
+       fi
+done
+
+
+<<comment
+for (( i=$2; i<=$3; i++))
+do
+	rm -r $1$i
+done
+comment
+#ls 
+
+rm -r day*
+
+
+
+
